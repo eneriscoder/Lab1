af4c9fec2c2880408dc3379de0b4aa04215376b8
Васильев Денис <enerispro@gmail.com>
2025-09-10 15:22:43 +0300

    Bezier curve addition

---
+++ curve.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import math as math
+import numpy as np
+
+def binom_coef(n,k):
+    return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
+
+def curve(points, amount, dimension):
+
+    # visual test
+    # curve_pt.append(start)
+    # curve_pt.append(mid)
+    # curve_pt.append(end)
+    # curve_pt.append(start)
+    t = np.linspace(0, 1, amount)
+    n=  len(points) - 1
+    curve_pt = np.zeros((amount, dimension))
+
+    for i in range(len(points)):
+
+        bernstein =  binom_coef(n, i) * (t**i) * ((1 - t)**(n - i))
+        curve_pt += np.outer(bernstein, points[i])
+    #print(curve_pt)
+    return curve_pt
+
+actual_points = np.array([[0,10], [4, -20],[7, 50] , [11, -20], [13,10]])
+cords = curve( actual_points, 1000, 2)
+
+plt.figure(figsize=(8, 6))             # Create a figure containing a single Axes.
+plt.plot(actual_points[:, 0], actual_points[:, 1], 'r--')
+plt.plot(cords[:, 0], cords[:, 1], 'b-')  # Plot some data on the Axes.
+plt.show()
